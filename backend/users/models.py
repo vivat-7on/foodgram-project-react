95fe5220b698d37fd808563f9a12b82ef4928e8a
Semenov Valerii <vivat-7on@yandex.ru>
2024-02-20 16:55:36 +0300

    Fix CustomUser model and add Subscribe model.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,20 +1,23 @@
-from django.contrib.auth.models import (AbstractBaseUser,
-                                        BaseUserManager,
-                                        PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractUser,
+    UserManager,
+)
 from django.db import models
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError('Введите адрес электронной почты!')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, username=None, password=None,
+                         **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -26,19 +29,38 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(verbose_name='email address', unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_subscribed = models.BooleanField(verbose_name='подписан', default=False)
-
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
+    email = models.EmailField(
+        unique=True,
+    )
+    is_subscribed = models.BooleanField(
+        default=False,
+    )
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Subscribe(models.Model):
+    subscriber = models.ForeignKey(
+        CustomUser,
+        related_name='подписчик',
+        on_delete=models.CASCADE
+    )
+    subscribed_to = models.ForeignKey(
+        CustomUser,
+        related_name='подписка',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ('subscriber', 'subscribed_to')
+
+    def __str__(self):
+        return f"{self.subscriber} -> {self.subscribed_to}"
