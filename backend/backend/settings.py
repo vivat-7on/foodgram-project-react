c0b48d6a1034910c89e75f4b7e5d2d96244060bf
Semenov Valerii <vivat-7on@yandex.ru>
2024-02-26 10:44:49 +0300

    Start app.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework.authtoken',
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers
 from django.conf.urls.static import static
-
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
