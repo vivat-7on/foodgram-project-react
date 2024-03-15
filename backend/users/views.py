1d9885eb776b3496935887b903d9a3a029903e1c
Semenov Valerii <vivat-7on@yandex.ru>
2024-03-15 21:46:02 +0300

    Fix subscriptions_list_detail_delete.

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
+from django.db import IntegrityError, transaction
 
 from .models import CustomUser, Subscribe
 from .serializers import SubscriptionSerializer
@@ -25,7 +25,7 @@ class CustomUserViewSet(UserViewSet):
             subscribed_to_id = sub.subscribed_to.id
             users = CustomUser.objects.filter(pk=subscribed_to_id)
             paginator = PageNumberPagination()
-            paginator.page_size = 1
+            paginator.page_size = 3
             result_page = paginator.paginate_queryset(users, request)
             serializer = SubscriptionSerializer(result_page, many=True)
             return paginator.get_paginated_response(serializer.data)
@@ -33,20 +33,34 @@ class CustomUserViewSet(UserViewSet):
     @action(detail=True, permission_classes=[IsAuthenticated])
     def subscriptions_detail(self, request, id=None):
         subscriber = self.request.user
-        subscribed_to = CustomUser.objects.filter(pk=id)
-        for sub in subscribed_to:
-            Subscribe.objects.create(
-                subscriber=subscriber,
-                subscribed_to=sub
-            ).save()
-            serializer = SubscriptionSerializer(subscribed_to, many=True)
-            return Response(serializer.data)
+        try:
+            subscribed_to = CustomUser.objects.get(pk=id)
+            with transaction.atomic():
+                Subscribe.objects.create(
+                    subscriber=subscriber,
+                    subscribed_to=subscribed_to
+                )
+        except CustomUser.DoesNotExist:
+            return HttpResponse('Автор не найден.', status=400)
+        except IntegrityError:
+            return HttpResponse(
+                'Вы уже подписанны на этого автора.',
+                status=400
+            )
+        serializer = SubscriptionSerializer(subscribed_to)
+        return Response(serializer.data)
 
     @action(detail=True, permission_classes=[IsAuthenticated])
     def subscriptions_delete(self, request, id=None):
         subscriber = self.request.user
-        Subscribe.objects.filter(
-            subscriber=subscriber,
-            subscribed_to=id
-        ).delete()
+        try:
+            subscribed_to = CustomUser.objects.get(pk=id)
+            with transaction.atomic():
+                Subscribe.objects.filter(
+                    subscriber=subscriber,
+                    subscribed_to=subscribed_to
+                ).delete()
+        except CustomUser.DoesNotExist:
+            return HttpResponse('Автор не найден.', status=400)
+
         return HttpResponse(status=204)
