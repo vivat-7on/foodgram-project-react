b6904e68d8fd194503bf5072fae32d89b43446d1
Semenov Valerii <vivat-7on@yandex.ru>
2024-04-15 12:51:00 +0300

    Add filters in recipe

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.db import transaction, IntegrityError
-from django.http import Http404, HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.decorators import action
+from rest_framework.exceptions import ParseError
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
@@ -15,7 +16,11 @@ from rest_framework.viewsets import (
     ReadOnlyModelViewSet,
     ModelViewSet,
 )
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 from rest_framework import filters
 
 from .serializers import (
@@ -54,6 +59,42 @@ class IngredientViewSet(ReadOnlyModelViewSet):
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    filter_backends = (filters.SearchFilter,)
+    filterset_fields = ['author__id', 'tags__name']
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author = self.request.query_params.get('author', None)
+        if author is not None:
+            queryset = queryset.filter(author__id=author)
+
+        tags = self.request.query_params.getlist('tags', [])
+        if tags:
+            queryset = queryset.filter(tags__slug__in=tags)
+
+        is_favorited = self.request.query_params.get('is_favorited', '0')
+        try:
+            if is_favorited == '1' and self.request.user.is_authenticated:
+                favorited_recipes = FavoriteRecipe.objects.filter(
+                    user=self.request.user
+                ).values_list('recipe_id', flat=True)
+                queryset = queryset.filter(id__in=favorited_recipes)
+        except (ValueError, TypeError) as e:
+            raise ParseError(detail='Invalid value for is_favorited')
+
+        is_in_shopping_cart = self.request.query_params.get(
+            'is_in_shopping_cart', '0')
+        try:
+            if is_in_shopping_cart == '1' \
+                    and self.request.user.is_authenticated:
+                shopping_cart_recipes = ShoppingCard.objects.filter(
+                    user=self.request.user
+                ).values_list('recipe_id', flat=True)
+                queryset = queryset.filter(id__in=shopping_cart_recipes)
+        except (ValueError, TypeError) as e:
+            raise ParseError(detail='Invalid value for is_in_shopping_cart')
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
